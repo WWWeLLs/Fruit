f4f1cb0ce5491881a39fff1dd80011b526ac7851
Wennona <50216937+WWWeLLs@users.noreply.github.com>
2019-05-31 15:38:57 -0500

    Add files via upload

---
+++ Fruit/Fruit/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Fruit
+{
+    public class Fruit
+    //CREATE NEW CLASS CALLED FRUIT
+    //Your new class file will house your parent class and any subclasses that you create
+    {
+        //CREATE FRUIT CHARACTER PROPERTIES
+        public string _name { set; get; }
+
+        public string _type { set; get; }
+        public string _color { set; get; }
+
+        //CREATE FRUIT CONSTRUCTOR/ PARAMETERS
+        public Fruit(string name, string type, string color)
+        {
+            _name = name;
+            _type = type;
+            _color = color;
+            Console.WriteLine("Fruit becomes real Characters");
+        }
+
+        //FRUIT METHOD 1
+        public void flavor(string flavorBurst)
+        {
+            Console.WriteLine("Hello my flavor {0}", _name, flavorBurst);
+        }
+    }
+
+    public class Pineapple : Fruit
+    {
+        //PROPERTIES
+        public string _emotion { set; get; }
+
+        //CONSTRUCTOR
+        public Pineapple(string name, string type, string color, string emotion) : base(name, type, color)
+        {
+            _name = name;
+            _type = type;
+            _color = color;
+            _emotion = emotion;
+        }
+
+        public void loving(string loving)
+        {
+            Console.WriteLine("Hello my I love {0}", _name, loving);
+        }
+
+
+
+    }
+}
---
+++ Fruit/Fruit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fruit
+{
+    class Program  
+    {
+        static void Main(string[] args)
+        {           //NEW METHOD USED FLAVOR & LOVING
+            Pineapple PA = new Pineapple("Mrs. Pineknickle", "papple", "yellow", "happy");
+            PA.flavor("Mrs. Pineapple smells sweet");
+            PA.loving("Mrs. Pineapple hair is shaped like a flower");
+          
+        }
+    }
+}
